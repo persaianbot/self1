8115e9b33e9438a72306a2433eaab7b72ebbb1bf
persaianbot <mrx2121mrx@gmail.com>
2016-03-26 05:31:38 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -253,7 +253,7 @@ function create_config( )
     "me",
     "qrcode"
     },
-    sudo_users = {164985762},--Sudo users
+    sudo_users = {137264699},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[avast v5 - 
